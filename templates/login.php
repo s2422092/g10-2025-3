cdcd78954b8ead12a0df0e9c983c12049a97d80c
Teramoto1127 <s2522028@stu.musashino-u.ac.jp>
2025-11-07 16:28:28 +0900

    再来週 login.phpの編集からスタート

--- templates/login.php
+++ templates/login.php
@@ -4,11 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Diary login</title>
+    <title>一言×色日記 - ログイン</title>
 </head>
 
 <body>
     <h1>ログイン画面</h1>
+    <form>
+        <label>ユーザー名：
+            <input type="text" name="username" required>
+        </label>
+        <br>
+        <label>パスワード：
+            <input type="password" name="password" required>
+        </label>
+</br>
+
+        <button type="submit">ログイン</button>
+    </form>
 
 </body>
 </html>
